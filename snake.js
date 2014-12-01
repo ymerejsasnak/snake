964155074c061229451c4a652573a453910e65c1
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-12-01 11:06:19 -0500

    snake initializes

--- snake.js
+++ snake.js
@@ -1,16 +1,24 @@
 //these are intended to act as constants for readability of code
 var GRID_SIZE = 40;
+
 var EMPTY = " ";
+var HEAD = "O";
+var BODY = "o";
+
+var RIGHT = "r";
+var LEFT = "l";
+var UP = "u";
+var DOWN = "d";
 
 
 
-function initializeGrid(grid) {
+
+function initializeGrid() {
 	// grid will be object in format of coordinate pair as key, item in that space as value denoted by character: {x,y: char}  ie {1,1: " "}
   
-  //clear grid
-  grid = {};
+  var grid = {};
 
-  //create empty grid
+  //fill empty grid
   for (x = 0; x < GRID_SIZE; x++) {
   	for (y = 0; y < GRID_SIZE; y++) {
   		grid[x + "," + y] = EMPTY;
@@ -18,6 +26,18 @@ function initializeGrid(grid) {
   }
 
   return grid;
+}
+
+
+
+function initializeSnake() {
+	var snake = {
+	  headPosition: [20,20],
+	  direction: RIGHT,
+	  body: [[20,20]],
+  };
+  
+  return snake;
 }
 
 
@@ -41,10 +61,16 @@ function render(grid) {
 
 
 
+
 $(document).ready(function() {
   
-  var grid = initializeGrid(grid);
 
+  var grid = initializeGrid();
+  var snake = initializeSnake();
+ 
+  
+  grid[snake.headPosition.join()] = HEAD;
   render(grid);
 
-})
+
+});
